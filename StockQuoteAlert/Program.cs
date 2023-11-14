515cb0f32a1da1a8bcd59b1c6135798fb1ef223d
natan <natan@AZARIA>
2023-11-13 22:50:10 -0300

    Add project files.

---
+++ StockQuoteAlert/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using NamespaceStock;
+using NamespaceStockAPI;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string stock = "PETR4";
+        string key = "eTD1U371z4ybsxyDhkCCDg";
+
+        var APIHandler = new StockAPI(key);
+        Stock body = await APIHandler.fetch(stock);
+        Console.WriteLine(body.ToString()); 
+    }
+}
---
+++ StockQuoteAlert/StockAPI.cs
@@ -0,0 +1,41 @@
+﻿using NamespaceStock;
+using System.Text.Json.Nodes;
+
+namespace NamespaceStockAPI;
+
+internal class StockAPI
+{
+    private string _key;
+    private HttpClient _httpClient;
+
+    public StockAPI(string key)
+    {
+        _key = key;
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<Stock> fetch(string stock)
+    {
+        string content = "";
+        string url = makeUrl(stock);
+
+        try
+        {
+            using var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            content = await response.Content.ReadAsStringAsync();
+        } 
+        catch (Exception ex) 
+        {
+            Console.WriteLine($"Failed to fetch data from {stock}: {ex.Message}");
+        }
+
+        var parsed = JsonNode.Parse(content)!["results"]![0]!;
+        
+        decimal price = parsed["regularMarketPrice"]!.GetValue<decimal>();
+        string currency = parsed["currency"]!.GetValue<string>();
+        return new Stock(stock, price, currency);
+    }
+
+    private string makeUrl(string stock) => String.Format("https://brapi.dev/api/quote/{0}?token={1}", stock, _key);
+}
